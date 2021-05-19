9f74657c0582ae8e12f048a5d403f9e480a6dd77
Firas Moosvi <firas@moosvi.com>
2021-05-18 23:18:44 -0700

    Fix body part split and revamp to add md-to-pl functions (#3)
    * added a new function to convert defdict to regular dict
    
    * Revert "added a new function to convert defdict to regular dict"
    
    This reverts commit 485a43fc726e0167ee26329936ef7b34ff48cb89.
    
    * update to add a function to convert defdict to regular dict; and also add body_split
    
    * minor changes
    
    * add gitignore file to remove data
    
    * Add >= to assert statement
    
    * move functions from md-to-pl to this package and add prairielearn.py

--- src/problem_bank_scripts/problem_bank_scripts.py
+++ src/problem_bank_scripts/problem_bank_scripts.py
@@ -4,21 +4,44 @@
 
 from docopt import docopt
 
-## Imports
-
-# Loading and Saving files & others
+# Imports
+## Loading and Saving files & others
+import uuid
+import json
+from src.problem_bank_scripts import prairielearn as pl
 import pathlib
 import sys
 import numpy as np
 import os
 from collections import defaultdict
 
-# Parse Markdown
+## Parse Markdown
 from markdown_it import MarkdownIt # pip install markdown-it-py 
 from mdformat.renderer import MDRenderer # pip install mdformat
 
-# Dealing with YAML
+## Dealing with YAML
 import yaml
+
+# Start of reading/parsing functions
+
+def defdict_to_dict(defdict, finaldict):
+    """Convert a defaultdict (nested) to a regular dictionary.
+        - Answer copied from: https://stackoverflow.com/a/61133504/2217577
+    Args:
+        defdict (dict): defaultdict
+        finaldict (dict): regular dictionary
+
+    Returns:
+        dict: Convert to regular dictionary
+    """
+    # pass in an empty dict for finaldict
+    for k, v in defdict.items():
+        if isinstance(v, defaultdict):
+            # new level created and that is the new value
+            finaldict[k] = defdict_to_dict(v, {})
+        else:
+            finaldict[k] = v
+    return finaldict
 
 def rounded(num, digits_after_decimal = 2):
     """Rounds numbers properly to specified digits after decimal place
@@ -30,8 +53,6 @@ def rounded(num, digits_after_decimal = 2):
     Returns:
         str: A string that is correctly rounded (you know why it's not a float!)
     """
-
-
     """
     This needs to be heavily tested!!
     WARNING: This does not do sig figs yet!
@@ -69,7 +90,7 @@ def split_body_parts(num_parts,body_parts):
 
     parts_dict = nested_dict()
 
-    for pnum in range(num_parts):
+    for pnum in range(1,num_parts+1):
 
         part = 'part'+f'{pnum}'
         # Set up tokens by parsing the md file
@@ -84,7 +105,7 @@ def split_body_parts(num_parts,body_parts):
         parts_dict[part]['content'] = MDRenderer().render(tokens[ptt[1]+1:pa[0]], mdit.options, env)
         parts_dict[part]['answer']['content'] = MDRenderer().render(tokens[pa[1]+1:], mdit.options, env)
 
-    return parts_dict
+    return defdict_to_dict(parts_dict,{})
 
 def read_md_problem(filepath):
     """Reads a MystMarkdown problem file and returns a dictionary of the header and body
@@ -126,7 +147,8 @@ def read_md_problem(filepath):
     for x,t in enumerate(tokens):
 
         if t.tag == 'h1' and t.nesting == 1: # title
-            blocks['title'] = [x,]
+            # oh boy. this is going to break and it will be your fault firas.
+            blocks['title'] = [x,x+3]
             num_titles += 1
 
         elif t.tag == 'h2' and t.nesting == 1:
@@ -143,10 +165,13 @@ def read_md_problem(filepath):
 
     # Assert statements (turn into tests!)
     assert num_titles == 1, "I see {0} Level 1 Headers (#) in this file, there should only be one!".format(num_titles)
-    assert block_count > 1, "I see {0} Level 2 Headers (##) in this file, there should be at least 1".format(block_count -1)
+    assert block_count >= 1, "I see {0} Level 2 Headers (##) in this file, there should be at least 1".format(block_count -1)
 
     # Add the end of the title block; # small hack
-    blocks['title'].append(blocks['block1'][0])
+    #blocks['title'].append(blocks['block1'][0])
+
+    # Get the preamble before the parts start
+    blocks['preamble'] = [blocks['title'][1],blocks['block1'][0]]
 
     ## Process the blocks into markdown
 
@@ -160,6 +185,9 @@ def read_md_problem(filepath):
 
         if k == 'title':
             body_parts['title'] = rendered_part
+        
+        elif k == 'preamble':
+            body_parts['preamble'] = rendered_part
 
         elif 'Rubric' in rendered_part:
             body_parts['Rubric'] = rendered_part
@@ -174,10 +202,10 @@ def read_md_problem(filepath):
             part_counter +=1
             body_parts['part{0}'.format(part_counter)] = rendered_part
     
-    return {'header': header,
+    return defdict_to_dict({'header': header,
             'body_parts': body_parts,
             'num_parts': part_counter,
-            'body_parts_split': split_body_parts(part_counter,body_parts) }
+            'body_parts_split': split_body_parts(part_counter,body_parts) },{})
 
 def dict_to_md(md_dict, remove_keys = [None,]):
     """ Takes a nested dictionary (e.g. output of read_md_problem()) and returns a multi-line string  that can be written to a file (after removing specified keys).   
@@ -198,3 +226,218 @@ def dict_to_md(md_dict, remove_keys = [None,]):
             md_string += md_dict[k]
 
     return md_string
+
+## Functions from md-to-pl
+
+def write_info_json(output_path, parsed_question):
+    """
+    Args:
+        output_path (Path): [description]
+        parsed_question (dict]): [description]
+    """
+
+    pathlib.Path(output_path / 'info.json').write_text("""{
+            "uuid": \"""" + str(uuid.uuid4()) + """\",
+            "title": \"""" + parsed_question['header']['title'] + """",
+            "topic": \"""" + parsed_question['header']['topic'] + """",
+            "tags":  """ + json.dumps(parsed_question['header']['tags']) + """,
+            "type": "v3"
+        }""")
+
+def write_server_py(output_path,parsed_question):
+    """
+    Args:
+        output_path ([type]): [description]
+        parsed_question ([type]): [description]
+    """
+    
+    try:
+        imp, func = parsed_question['header']['server'].split('# Start problem code')
+    except:
+        raise
+
+    func = func.replace('read_csv("','read_csv(data["options"]["client_files_course_path"]+"/')
+    func = func.replace('\n','\n    ')
+
+    (output_path / "server.py").write_text(imp+'def generate(data):'+func)
+
+def process_multiple_choice(part_name,parsed_question, data_dict):
+    """Processes markdown format multiple-choice questions and returns PL HTML
+    Args:
+        output_path (Path): [description]
+        parsed_question (dict): [description]
+        data_dict (dict)
+    
+    Returns:
+        str: Multiple choice question is returned as a string with PL-compliant syntax.
+    """
+
+    html = f"""<pl-question-panel>\n\t<markdown>{parsed_question['body_parts_split'][part_name]['content']}\t</markdown>\n</pl-question-panel>\n\n"""
+    
+    pl_customizations = " ".join([f'{k} = "{v}"' for k,v in parsed_question['header'][part_name]['pl-customizations'].items()]) # PL-customizations
+    html += f"""<pl-multiple-choice answers-name="{part_name}_ans" {pl_customizations} >\n"""
+
+    if data_dict['params'][f'vars']['units']:
+        units = f"|@ params.vars.units @|"
+    else:
+        units = ''
+
+    for ans in data_dict['params'][f'{part_name}'].keys():
+        html += f"\t<pl-answer correct= |@ params.{part_name}.{ans}.correct @| > |@ params.{part_name}.{ans}.value @| {units} </pl-answer>\n"
+
+    html += '</pl-multiple-choice>\n' 
+
+    return replace_tags(html)
+
+def process_dropdown(part_name,parsed_question, data_dict):
+    """Processes markdown format dropdown questions and returns PL HTML
+
+    Args:
+        part_name (string): Name of the question part being processed (e.g., part1, part2, etc...)
+        parsed_question (dict): Dictionary of the MD-parsed question (output of `read_md_problem`)
+        data_dict (dict): Dictionary of the `data` dict created after running server.py using `exec()`
+
+    Returns:
+        html: A string of HTML that is part of the final PL question.html file.
+    """
+
+    raise NotImplementedError
+
+def process_number_input(part_name,parsed_question, data_dict):
+    """Processes markdown format number-input questions and returns PL HTML
+
+    Args:
+        part_name (string): Name of the question part being processed (e.g., part1, part2, etc...)
+        parsed_question (dict): Dictionary of the MD-parsed question (output of `read_md_problem`)
+        data_dict (dict): Dictionary of the `data` dict created after running server.py using `exec()`
+
+    Returns:
+        html: A string of HTML that is part of the final PL question.html file.
+    """
+
+    html = f"""<pl-question-panel>\n\t<markdown>{parsed_question['body_parts_split'][part_name]['content']}\t</markdown>\n</pl-question-panel>\n\n"""
+    
+    pl_customizations = " ".join([f'{k} = "{v}"' for k,v in parsed_question['header'][part_name]['pl-customizations'].items()]) # PL-customizations
+    html += f"""<pl-number-input answers-name="{part_name}_ans" {pl_customizations} ></pl-number-input>\n"""
+
+    return replace_tags(html)
+
+def process_checkbox(part_name,parsed_question, data_dict):
+    """Processes markdown format checkbox (select all that apply) questions and returns PL HTML
+
+    Args:
+        part_name (string): Name of the question part being processed (e.g., part1, part2, etc...)
+        parsed_question (dict): Dictionary of the MD-parsed question (output of `read_md_problem`)
+        data_dict (dict): Dictionary of the `data` dict created after running server.py using `exec()`
+
+    Returns:
+        html: A string of HTML that is part of the final PL question.html file.
+    """
+    # start with the MCQ version and then...change things for checkbox questions
+    html = process_multiple_choice(part_name,parsed_question, data_dict).replace('-multiple-choice','-checkbox')
+    return html
+
+def process_symbolic_input(part_name,parsed_question, data_dict):
+    """Processes markdown format symbolic questions and returns PL HTML
+
+    Args:
+        part_name (string): Name of the question part being processed (e.g., part1, part2, etc...)
+        parsed_question (dict): Dictionary of the MD-parsed question (output of `read_md_problem`)
+        data_dict (dict): Dictionary of the `data` dict created after running server.py using `exec()`
+
+    Returns:
+        html: A string of HTML that is part of the final PL question.html file.
+    """
+
+    html = f"""<pl-question-panel>\n\t<markdown>{parsed_question['body_parts_split'][part_name]['content']}\t</markdown>\n</pl-question-panel>\n\n"""
+    
+    pl_customizations = " ".join([f'{k} = "{v}"' for k,v in parsed_question['header'][part_name]['pl-customizations'].items()]) # PL-customizations
+    html += f"""<pl-symbolic-input answers-name="{part_name}_ans" {pl_customizations} ></pl-symbolic-input>\n"""
+
+    return replace_tags(html).replace('\\\\','\\')
+
+def replace_tags(string):
+    """Takes in a string with tags: |@ and @| and returns {{ and }} respectively. This is because Python strings can't have double curly braces.
+
+    Args:
+        string (str): String to be processed, can be multi-line.
+
+    Returns:
+        string (str): returns string with tags replaced with curly braces.
+    """
+    return string.replace('|@','{{').replace('@|','}}')
+
+def process_question(input_file, output_path):
+
+    try:
+        input_file = pathlib.Path(input_file)
+    except:
+        print("File does not exist.")
+        raise
+
+    output_path = pathlib.Path(output_path) / input_file.parts[-2]
+
+    # Parse the MD file
+    parsed_q = read_md_problem(input_file)
+
+    # Create output dir if it doesn't exist
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+
+    ############### Start Sketchiest Part
+    # Run the python code
+    exec(parsed_q['header']['server'].split('# Update the data object with a new dict')[0],globals())
+    ############### End Sketchiest Part
+
+    # Write info.json file
+    write_info_json(output_path, parsed_q)
+
+    ## Single part questions
+
+    if parsed_q['num_parts'] == 1:
+        q_type = parsed_q['header']['part1']['type']
+        if 'multiple-choice' in q_type:
+            question_html = process_multiple_choice('part1',parsed_q,data2)
+        elif 'number-input' in q_type:
+            question_html = process_number_input('part1',parsed_q,data2)
+        elif 'checkbox' in q_type:
+            question_html = process_checkbox('part1',parsed_q,data2)
+        elif 'symbolic-input' in q_type:
+            question_html = process_symbolic_input('part1',parsed_q,data2)
+        else:
+            raise NotImplementedError(f"This question type ({q_type}) is not yet implemented.")
+
+    ##### Multi part
+    else:
+        question_html = ""
+        for pnum in range(1, parsed_q['num_parts'] + 1):
+            part = 'part'+f'{pnum}'
+            q_type = parsed_q['header'][part]['type']
+
+            question_html += f"""
+                <div class="card my-2">
+                \t<div class="card-header">
+                \t{parsed_q['body_parts_split'][part]['title']}
+                </div>\n
+                <div class="card-body">\n\n\n
+                """
+            if 'multiple-choice' in q_type:                
+                question_html += f"{process_multiple_choice(part,parsed_q,data2)}"  
+            elif 'number-input' in q_type:
+                question_html += f"{process_number_input(part,parsed_q,data2)}"
+            elif 'checkbox' in q_type:
+                question_html += process_checkbox(part,parsed_q,data2)
+            elif 'symbolic-input' in q_type:
+                question_html += process_symbolic_input(part,parsed_q,data2)
+            else:
+                raise NotImplementedError(f"This question type ({q_type}) is not yet implemented.")
+
+            question_html += "</div>\n</div>\n\n"
+
+    # Write question.html file
+
+    (output_path / "question.html").write_text(question_html)
+
+    # Write server.py file
+    write_server_py(output_path,parsed_q)
+
+    # TODO: Find and move any image assets 
