030eaf7f74747605f4d864cba8d116da47d62299
Firas Moosvi <firas@moosvi.com>
2021-06-11 17:44:58 -0700

    increment version

--- tests/test_problem_bank_scripts.py
+++ tests/test_problem_bank_scripts.py
@@ -3,7 +3,7 @@ from src.problem_bank_scripts import problem_bank_scripts as pbs
 import pandas as pd
 
 def test_version():
-    assert __version__ == '0.0.10'
+    assert __version__ == '0.0.11'
 
 # def test_rounded():
 
