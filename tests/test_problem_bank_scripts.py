f0c1c8a864c11c213391ee01cad37e25b8fcc2d4
Akshat Poddar <akshatpoddar312002@gmail.com>
2021-06-16 12:37:49 -0700

    first working test

--- tests/test_problem_bank_scripts.py
+++ tests/test_problem_bank_scripts.py
@@ -1,9 +1,11 @@
 from problem_bank_scripts import __version__
 from src.problem_bank_scripts import problem_bank_scripts as pbs
 import pandas as pd
+import pathlib  
+import filecmp
 
 def test_version():
-    assert __version__ == '0.1.1'
+    assert __version__ == '0.0.7'
 
 # def test_rounded():
 
@@ -13,3 +15,9 @@ def test_version():
 
 #     assert rounded_value == str(33.333)
 
+def test_prairie():
+    inputDest = pathlib.Path('tests/test_question_templates/question_inputs/q01_multiple-choice/q01_multiple-choice.md')
+    outputDest = pathlib.Path('tests/test_question_templates/question_generated_outputs/q01_multiple-choice/q01_multiple-choice.md')
+    pbs.process_question_pl(inputDest, outputDest)
+    compareDest = pathlib.Path('tests/test_question_templates/question_expected_outputs/q01_multiple-choice/question.html')
+    assert filecmp.cmp(outputDest.parent / 'question.html', compareDest, shallow = False)
