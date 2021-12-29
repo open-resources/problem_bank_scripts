7c6551b1d8e6f1b6548f7a6d66083a1b5799bf49
Firas Moosvi <firas@moosvi.com>
2021-12-28 22:28:15 -0800

    add functionality for a longtext questions

--- src/problem_bank_scripts/problem_bank_scripts.py
+++ src/problem_bank_scripts/problem_bank_scripts.py
@@ -389,6 +389,25 @@ def process_symbolic_input(part_name,parsed_question, data_dict):
 
     return replace_tags(html).replace('\\\\','\\')
 
+def process_longtext(part_name,parsed_question, data_dict):
+    """Processes markdown format of long-text questions and returns PL HTML 
+    Args:
+        part_name (string): Name of the question part being processed (e.g., part1, part2, etc...)
+        parsed_question (dict): Dictionary of the MD-parsed question (output of `read_md_problem`)
+        data_dict (dict): Dictionary of the `data` dict created after running server.py using `exec()`
+
+    Returns:
+        html: A string of HTML that is part of the final PL question.html file.
+    """
+    pl_customizations = " ".join([f'{k} = "{v}"' for k,v in parsed_question['header'][part_name]['pl-customizations'].items()]) # PL-customizations
+
+    html = f"""<pl-question-panel>\n<markdown>{parsed_question['body_parts_split'][part_name]['content']}</markdown>\n</pl-question-panel>\n\n"""
+
+    html += f"""<pl-rich-text-editor { pl_customizations } > </pl-rich-text-editor>"""
+    print('this is actually working')
+
+    return replace_tags(html)
+
 def replace_tags(string):
     """Takes in a string with tags: |@ and @| and returns {{ and }} respectively. This is because Python strings can't have double curly braces.
 
@@ -602,6 +621,8 @@ def process_question_pl(source_filepath, output_path = None):
             question_html += process_symbolic_input('part1',parsed_q,data2)
         elif 'dropdown' in q_type:
             question_html += process_dropdown('part1',parsed_q,data2)
+        elif 'longtext' in q_type:
+            question_html += process_longtext('part1',parsed_q,data2)
         else:
             raise NotImplementedError(f"This question type ({q_type}) is not yet implemented.")
 
@@ -631,6 +652,8 @@ def process_question_pl(source_filepath, output_path = None):
                 question_html += process_symbolic_input(part,parsed_q,data2)
             elif 'dropdown' in q_type:
                 question_html += process_dropdown(part,parsed_q,data2)
+            elif 'longtext' in q_type:
+                question_html += process_longtext(part,parsed_q,data2)
             else:
                 raise NotImplementedError(f"This question type ({q_type}) is not yet implemented.")
 
