e0785a617aafb4a285d5e241ff916427098229ec
Firas Moosvi <firas@moosvi.com>
2022-06-05 01:35:15 -0700

    increment version

--- tests/test_problem_bank_scripts.py
+++ tests/test_problem_bank_scripts.py
@@ -7,7 +7,7 @@ import os
 import pytest
 
 def test_version():
-    assert __version__ == '0.3.3'
+    assert __version__ == '0.3.4'
 
 @pytest.fixture(scope="session", autouse= True)
 def add_random_seed(paths):
