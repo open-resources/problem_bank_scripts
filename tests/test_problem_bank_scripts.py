3a0bfa9e4517519f2d1eb9b06a512d8a24b69cdf
Firas Moosvi <firas@moosvi.com>
2022-09-17 19:51:49 -0700

    increment version

--- tests/test_problem_bank_scripts.py
+++ tests/test_problem_bank_scripts.py
@@ -7,7 +7,7 @@ import pytest
 
 
 def test_version():
-    assert __version__ == "0.4.5"
+    assert __version__ == "0.4.6"
 
 
 # TODO: excluding symbolic questions, needs to be fixed because of how sympy objects are handled
