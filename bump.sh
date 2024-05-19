79630299b6c6975e0bcafa41d6d552d1c0da6b0e
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2024-05-19 12:52:00 -0700

    Create version bump script

---
+++ bump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSION_LEVEL=$1
+DRY_RUN=$2
+# Bump the version and save poetry output to string
+ret=$(poetry version $VERSION_LEVEL $DRY_RUN)
+echo $ret
+FROM=$(echo $ret | awk '{ print $4; }')
+TO=$(echo $ret | awk '{ print $6; }')
+
+# change any references to the old version in the project
+echo "Bumping version from $FROM to $TO in __init__.py"
+sed -i "s/__version__ = \"$FROM\"/__version__ = \"$TO\"/g" src/problem_bank_scripts/__init__.py
+echo "Bumping version from $FROM to $TO in tests/test_problem_bank_scripts.py"
+sed -i "s/__version__ == \"$FROM\"/__version__ == \"$TO\"/g" tests/test_problem_bank_scripts.py
+
+# commit the changes
+git add src/problem_bank_scripts/__init__.py tests/test_problem_bank_scripts.py pyproject.toml
+git commit -m "Increment version from $FROM to $TO"
+
+# create a tag
+git tag -m "v$TO"
+
+# push the changes
+git push
+
