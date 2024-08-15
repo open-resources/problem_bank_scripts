55c34b50d8eecbc7db027524e518faee8863faf2
Gavin Kendal-Freedman <68259537+Bluesy1@users.noreply.github.com>
2024-08-15 10:42:03 -0700

    Update PBH version, deps, and docs (#120)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,10 +21,26 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
+    "matplotlib.sphinxext.plot_directive",
 ]
-napoleon_numpy_docstring = True
 nbsphinx_execute = "always"
 autodoc_member_order = "bysource"
+
+# Napoleon settings
+napoleon_numpy_docstring = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+
+plot_pre_code = """
+import numpy as np
+import matplotlib.pyplot as plt
+import problem_bank_helpers as pbh
+"""
+plot_formats = ["png"]
+plot_include_source = True
+plot_html_show_source_link = False
+plot_html_show_formats = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -34,7 +50,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # Links used for cross-referencing stuff in other documentation
 intersphinx_mapping = {
     "py": ("https://docs.python.org/3", None),
-    "matplotlib": ("https://matplotlib.org/stable", None),
+    "mpl": ("https://matplotlib.org/stable", None),
 }
 
 show_warning_types = True
@@ -43,17 +59,6 @@ suppress_warnings = ["docutils"]
 nitpick_ignore = [
     ("py:class", "optional"),
     ("py:class", "number"),
-    ("py:class", "float/str"),
-    ("py:class", "color"),
-    ("py:class", "x; μ"),
-    ("py:class", "\u03C3"),
-    ("py:class", "'"),
-]
-
-nitpick_ignore_regex = [
-    ("py:class", r".*2 floats"),
-    ("py:class", r"\d"),
-    ("py:class", r"default:.*"),
 ]
 
 # -- Options for HTML output -------------------------------------------------
